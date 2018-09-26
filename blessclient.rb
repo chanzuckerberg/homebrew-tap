1d429c49f0191cf3a6bc4ea9bb82f8a847f1a70c
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-26 16:30:08 -0700

    Brew formula update for blessclient version v0.2.0

--- blessclient.rb
+++ blessclient.rb
@@ -1,9 +1,9 @@
 class Blessclient < Formula
   desc "SSH without pain."
   homepage "https://github.com/chanzuckerberg/blessclient"
-  url "https://github.com/chanzuckerberg/blessclient/releases/download/v0.1.1/blessclient_0.1.1_darwin_amd64.tar.gz"
-  version "0.1.1"
-  sha256 "798b8ed619d95b5637e5ed561ff7515cecc3735d3b8643f23eb2dbc500b73bb8"
+  url "https://github.com/chanzuckerberg/blessclient/releases/download/v0.2.0/blessclient_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "a43745506d7403c2119aa4b1e49c06db18c9df55d6d4e9900deb0dafd9682c65"
 
   def install
     bin.install "blessclient"

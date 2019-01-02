3db833aa090accab7f494be5e7cde70719c5df4e
Ryan King <rking@chanzuckerberg.com>
2019-01-02 15:33:07 -0800

    Revert "Brew formula update for blessclient version v0.3.10"
    This reverts commit 459d11ff3fe35a50736af65d6ee026e7f60c52eb.

--- blessclient.rb
+++ blessclient.rb
@@ -1,9 +1,9 @@
 class Blessclient < Formula
   desc "SSH without pain."
   homepage "https://github.com/chanzuckerberg/blessclient"
-  url "https://github.com/chanzuckerberg/blessclient/releases/download/v0.3.10/blessclient_0.3.10_darwin_amd64.tar.gz"
-  version "0.3.10"
-  sha256 "54b311a8cdb95f47fb3bb42782b00b99d056fbeea22c786e51c88b925d82a483"
+  url "https://github.com/chanzuckerberg/blessclient/releases/download/v0.3.9/blessclient_0.3.9_darwin_amd64.tar.gz"
+  version "0.3.9"
+  sha256 "13b955b0f137584503c448172a532388fedcbe525937965ab7e0020546203715"
 
   def install
     bin.install "blessclient"

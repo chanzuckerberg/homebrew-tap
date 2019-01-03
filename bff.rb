d62df746ec722c935ec683df497cc2152d60571f
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-03 11:31:48 -0800

    Brew formula update for bff version v0.1.3

--- bff.rb
+++ bff.rb
@@ -1,9 +1,9 @@
 class Bff < Formula
   desc "Breaking, Feature, Fix - a tool for managing semantic versioning"
   homepage "https://github.com/chanzuckerberg/bff"
-  url "https://github.com/chanzuckerberg/bff/releases/download/v0.1.1/bff_0.1.1_darwin_amd64.tar.gz"
-  version "0.1.1"
-  sha256 "b5e782dcf5416eb1040a2c49cd3d2bace297f9929f7c3aaad24b0eacae67aec5"
+  url "https://github.com/chanzuckerberg/bff/releases/download/v0.1.3/bff_0.1.3_darwin_amd64.tar.gz"
+  version "0.1.3"
+  sha256 "09c68dcf3ee204841386a414336cdb903d6f9270441a63d6e37616fc2449e6a8"
 
   def install
     bin.install "bff"

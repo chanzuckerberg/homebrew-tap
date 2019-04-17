37acb53bb96969b6247af0bc877d5bdde81f75b5
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-17 14:55:06 -0700

    Brew formula update for fogg version v0.19.7

--- fogg.rb
+++ fogg.rb
@@ -1,9 +1,9 @@
 class Fogg < Formula
   desc "Terraform without pain."
   homepage "https://github.com/chanzuckerberg/fogg"
-  url "https://github.com/chanzuckerberg/fogg/releases/download/v0.19.6/fogg_0.19.6_darwin_amd64.tar.gz"
-  version "0.19.6"
-  sha256 "822671d8f9710d1d0cbae28e99734c2569523e5426b8e25fc8a51141d928914b"
+  url "https://github.com/chanzuckerberg/fogg/releases/download/v0.19.7/fogg_0.19.7_darwin_amd64.tar.gz"
+  version "0.19.7"
+  sha256 "5ba83a4733ca60ed8b4d5bd3756e91afc052348ea621cd441fb356972bed6232"
 
   def install
     bin.install "fogg"

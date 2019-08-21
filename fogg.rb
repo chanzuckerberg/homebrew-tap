61f7687114e86656228cb263bf135c629699d4cd
goreleaserbot <goreleaser@carlosbecker.com>
2019-08-21 11:10:04 -0700

    Brew formula update for fogg version v0.30.2

--- fogg.rb
+++ fogg.rb
@@ -2,16 +2,16 @@
 class Fogg < Formula
   desc "Terraform without pain."
   homepage "https://github.com/chanzuckerberg/fogg"
-  version "0.30.1"
+  version "0.30.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chanzuckerberg/fogg/releases/download/v0.30.1/fogg_0.30.1_darwin_amd64.tar.gz"
-    sha256 "01799ecfee8351ccb5fe2df76c75d07175ccfee1b374ce448dc378d31d20d8c9"
+    url "https://github.com/chanzuckerberg/fogg/releases/download/v0.30.2/fogg_0.30.2_darwin_amd64.tar.gz"
+    sha256 "c4aedf71d338cbc82618bbb8e062edcdf5f020eb92fc9ee01f0d4a5a19cf030b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chanzuckerberg/fogg/releases/download/v0.30.1/fogg_0.30.1_linux_amd64.tar.gz"
-      sha256 "88f9bb4582ea890818aac3a9dec37485ad7aa4187e05f76daf13458ce6cb6a95"
+      url "https://github.com/chanzuckerberg/fogg/releases/download/v0.30.2/fogg_0.30.2_linux_amd64.tar.gz"
+      sha256 "4b964b124d658a26570d654aac2491c8ee9236d37d8e568e33272c4eaffb9fc1"
     end
   end
 

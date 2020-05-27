e6f3efc5899954a2192b63c43950e7458807f06a
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-27 11:21:16 -0700

    Brew formula update for aws-oidc version v0.4.0

--- aws-oidc.rb
+++ aws-oidc.rb
@@ -2,16 +2,16 @@
 class AwsOidc < Formula
   desc "A command line utility tool to help generate AWS STS credentials from an OIDC application."
   homepage "https://github.com/chanzuckerberg/aws-oidc"
-  version "0.3.0"
+  version "0.4.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chanzuckerberg/aws-oidc/releases/download/v0.3.0/aws-oidc_0.3.0_darwin_amd64.tar.gz"
-    sha256 "f48f15285400cf137b686423916382ca8753cd04f2494fd49702759612160a9e"
+    url "https://github.com/chanzuckerberg/aws-oidc/releases/download/v0.4.0/aws-oidc_0.4.0_darwin_amd64.tar.gz"
+    sha256 "d4a84fbb7d41a812f21f69898b3ff727e9582a6dfbd1163c1276e5c394371b35"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chanzuckerberg/aws-oidc/releases/download/v0.3.0/aws-oidc_0.3.0_linux_amd64.tar.gz"
-      sha256 "a7e3780f28f2cddcf81a6b50e1ac7390babc0450946de6dbbbd2bb68a824b19b"
+      url "https://github.com/chanzuckerberg/aws-oidc/releases/download/v0.4.0/aws-oidc_0.4.0_linux_amd64.tar.gz"
+      sha256 "bc74fd860cd9e398b470c7d878719e80a0e780dc50afdac6aaa52366354e021e"
     end
   end
 

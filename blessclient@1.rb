72917cb8aa38577c3ed0f660c953d8ec9b322d38
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-20 12:52:37 -0400

    Brew formula update for blessclient version v1.4.0

--- blessclient@1.rb
+++ blessclient@1.rb
@@ -2,16 +2,16 @@
 class BlessclientAT1 < Formula
   desc "SSH without pain."
   homepage "https://github.com/chanzuckerberg/blessclient"
-  version "1.3.0"
+  version "1.4.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chanzuckerberg/blessclient/releases/download/v1.3.0/blessclient_1.3.0_darwin_amd64.tar.gz"
-    sha256 "a3798c99c48426a49aa0e26849323faba833439b052f723dd9f4ae0e2b3e2f2f"
+    url "https://github.com/chanzuckerberg/blessclient/releases/download/v1.4.0/blessclient_1.4.0_darwin_amd64.tar.gz"
+    sha256 "6d536e43c76187b55e7996d64e31ce215c1fc79b78485b71e3c3844270d5100b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chanzuckerberg/blessclient/releases/download/v1.3.0/blessclient_1.3.0_linux_amd64.tar.gz"
-      sha256 "14033371c9517362cbe545650a8ba6b75899d6b6be07f475b53ee670e93f1316"
+      url "https://github.com/chanzuckerberg/blessclient/releases/download/v1.4.0/blessclient_1.4.0_linux_amd64.tar.gz"
+      sha256 "fbdc89171efb04ca8fc8eb87b4f2bafd394b77a7dee96b2a62ee57cf2f25f92f"
     end
   end
 

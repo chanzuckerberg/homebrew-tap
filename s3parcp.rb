4fb117a29ed680a59613709b8e605921f695d841
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-19 14:02:32 -0400

    Brew formula update for s3parcp version v0.2.1-test-17

--- s3parcp.rb
+++ s3parcp.rb
@@ -2,22 +2,23 @@
 class S3parcp < Formula
   desc "Copy large files to, from, and between s3 buckets with parallelism"
   homepage "https://github.com/chanzuckerberg/s3parcp"
-  version "0.2.1-test-16"
+  version "0.2.1-test-17"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chanzuckerberg/s3parcp/releases/download/v0.2.1-test-16/s3parcp_0.2.1-test-16_Darwin_x86_64.tar.gz"
-    sha256 "ffa05f9fa1d5c2e81eb1d35418c4ec31b1733cad5b935abaf3efc52eb814519c"
+    url "https://github.com/chanzuckerberg/s3parcp/releases/download/v0.2.1-test-17/s3parcp_0.2.1-test-17_Darwin_x86_64.tar.gz"
+    sha256 "ba01b09a110fc63c83c7feb140452d10e19872351039156fee9698e32fabff5c"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chanzuckerberg/s3parcp/releases/download/v0.2.1-test-16/s3parcp_0.2.1-test-16_Linux_x86_64.tar.gz"
-      sha256 "3780b9f79e37b37367f27630526b72e5bd0eae1a4cf2444cee53bbfc9900ffef"
+      url "https://github.com/chanzuckerberg/s3parcp/releases/download/v0.2.1-test-17/s3parcp_0.2.1-test-17_Linux_x86_64.tar.gz"
+      sha256 "bbefaee7b797e629d0063204572d939ab76e90e2b54ea9125c5b254fef4ebdad"
     end
   end
 
   def install
     bin.install "s3parcp" |
-    bash_completion.install "completions/bash"
+    include.install "utils.bash" |
+    bash_completion.install "completions/bash => "s3parcp"
   end
 
   test do

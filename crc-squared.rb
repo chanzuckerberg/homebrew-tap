fdf7403707bbca4c05b93adb3bf1bcabbed28b1e
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-08 09:50:50 -0700

    Brew formula update for crc-squared version v0.1.0

--- crc-squared.rb
+++ crc-squared.rb
@@ -2,16 +2,16 @@
 class CrcSquared < Formula
   desc "Light speed crc32c checksums"
   homepage "https://github.com/chanzuckerberg/crc-squared"
-  version "0.1.1"
+  version "0.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chanzuckerberg/crc-squared/releases/download/v0.1.1/crc-squared_0.1.1_darwin_amd64.tar.gz"
-    sha256 "ed12268b22a20dc59c98adf737d3673e8f83818b56e0ca386e1b1e76da4471f0"
+    url "https://github.com/chanzuckerberg/crc-squared/releases/download/v0.1.0/crc-squared_0.1.0_darwin_amd64.tar.gz"
+    sha256 "8b3196f290eeff0bedc223b4ae1db1fe2ac532387ed3626517ee55f233e822d8"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chanzuckerberg/crc-squared/releases/download/v0.1.1/crc-squared_0.1.1_linux_amd64.tar.gz"
-      sha256 "e7db23debc3ce16496e8b36bffe542d963dce291092de96c4727d7e9581f80af"
+      url "https://github.com/chanzuckerberg/crc-squared/releases/download/v0.1.0/crc-squared_0.1.0_linux_amd64.tar.gz"
+      sha256 "667f1adb4550ed8521305d2c2f66549c696e83bde2fd501d3d6abdd20e1f7957"
     end
   end
 

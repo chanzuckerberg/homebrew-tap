f7210059be74ca8793585c701fb3372c6445c39d
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-14 13:44:13 -0500

    Brew formula update for fogg version v0.53.1

--- fogg.rb
+++ fogg.rb
@@ -2,17 +2,16 @@
 class Fogg < Formula
   desc "Terraform without pain."
   homepage "https://github.com/chanzuckerberg/fogg"
-  version "0.53.0"
+  version "0.53.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chanzuckerberg/fogg/releases/download/v0.53.0/fogg_0.53.0_darwin_amd64.tar.gz"
-    sha256 "a1fcaeb733de202aa94c986db0195d55cacc32330d77b635718c457049c7f4dc"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/chanzuckerberg/fogg/releases/download/v0.53.0/fogg_0.53.0_linux_amd64.tar.gz"
-      sha256 "64b0d6a2f54e4a27f2fa559f70943cd549ad765112d03f754f28a25c15ddf0d6"
-    end
+    url "https://github.com/chanzuckerberg/fogg/releases/download/v0.53.1/fogg_0.53.1_darwin_amd64.tar.gz"
+    sha256 "c7a33d08bd77f2dc27e96846ba7df0f4ed715eb59f2de974e90efed8d0078f27"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/chanzuckerberg/fogg/releases/download/v0.53.1/fogg_0.53.1_linux_amd64.tar.gz"
+    sha256 "612ef086b5d4f4c42a4fd9ad21996ae531c5a40a45804f54ec2e9835e7aec4a0"
   end
 
   def install

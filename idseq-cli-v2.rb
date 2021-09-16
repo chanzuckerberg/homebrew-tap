ded34f825f171610f18b33ab35f40802b1b89396
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-16 15:03:12 -0700

    Brew formula update for idseq-cli-v2 version v2.2.3

--- idseq-cli-v2.rb
+++ idseq-cli-v2.rb
@@ -5,24 +5,24 @@
 class IdseqCliV2 < Formula
   desc "A CLI for uploading samples to IDSeq"
   homepage "https://github.com/chanzuckerberg/idseq-cli-v2"
-  version "2.2.2"
+  version "2.2.3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/chanzuckerberg/idseq-cli-v2/releases/download/v2.2.2/idseq-cli-v2_darwin_amd64.tar.gz"
-      sha256 "759bf3f8af3d6c1df1fa5964947a292151bb67e42bd23a36b54d97f93b85806a"
+      url "https://github.com/chanzuckerberg/idseq-cli-v2/releases/download/v2.2.3/idseq-cli-v2_darwin_amd64.tar.gz"
+      sha256 "1bb953bcc5e92a4c03e740378a954f0d608f3cbfa970f80413784f3a0ee2fc10"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/chanzuckerberg/idseq-cli-v2/releases/download/v2.2.2/idseq-cli-v2_darwin_arm64.tar.gz"
-      sha256 "2569939507a0eb7ebf3d2ab6ffa8b6b1a3e327ecde6f35c8e6b1c23da6ec408f"
+      url "https://github.com/chanzuckerberg/idseq-cli-v2/releases/download/v2.2.3/idseq-cli-v2_darwin_arm64.tar.gz"
+      sha256 "67c62967d4e32f9d22c77696d239ce659368c98472d1b3b60cc81b5d88186f39"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/chanzuckerberg/idseq-cli-v2/releases/download/v2.2.2/idseq-cli-v2_linux_amd64.tar.gz"
-      sha256 "dd821bcfaf61dd5b5194773497ef5efd1ea00f420e1208907ee9dae50006c962"
+      url "https://github.com/chanzuckerberg/idseq-cli-v2/releases/download/v2.2.3/idseq-cli-v2_linux_amd64.tar.gz"
+      sha256 "6c16de839e9f357b63a009596efa9b6c532fd928ef82d3c4e48c9544bc9ebc03"
     end
   end
 
